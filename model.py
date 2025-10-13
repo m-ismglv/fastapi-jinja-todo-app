02bf993d4fafe93aaac2b64c8601548fa0554d5d
Марат Исмагилов <ime2005@yandex.ru>
2025-10-13 13:12:10 +0500

    Практика 5: CRUD приложение с моделями ответов и обработкой ошибок

---
+++ model.py
@@ -0,0 +1,33 @@
+from pydantic import BaseModel
+from typing import List
+
+class TodoItem(BaseModel):
+    item: str
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "item": "Read the next chapter of the book"
+            }
+        }
+
+class Todo(BaseModel):
+    id: int
+    item: str
+
+class TodoItems(BaseModel):
+    todos: List[TodoItem]
+    
+    class Config:
+        schema_extra = {
+            "example": {
+                "todos": [
+                    {
+                        "item": "Example schema 1!"
+                    },
+                    {
+                        "item": "Example schema 2!"
+                    }
+                ]
+            }
+        }
