f1f0b33073d6803eb0de6921db982b163e4d5b54
Марат Исмагилов <ime2005@yandex.ru>
2025-10-20 14:16:00 +0500

    Practice 6: FastAPI with Jinja2 templates

--- model.py
+++ model.py
@@ -1,5 +1,6 @@
 from pydantic import BaseModel
-from typing import List
+from typing import List, Optional
+from fastapi import Form
 
 class TodoItem(BaseModel):
     item: str
@@ -12,20 +13,26 @@ class TodoItem(BaseModel):
         }
 
 class Todo(BaseModel):
-    id: int
+    id: Optional[int] = None
     item: str
 
+    @classmethod
+    def as_form(cls, item: str = Form(...)):
+        return cls(item=item)
+
 class TodoItems(BaseModel):
-    todos: List[TodoItem]
-    
+    todos: List[Todo]
+
     class Config:
         schema_extra = {
             "example": {
                 "todos": [
                     {
+                        "id": 1,
                         "item": "Example schema 1!"
                     },
                     {
+                        "id": 2, 
                         "item": "Example schema 2!"
                     }
                 ]
